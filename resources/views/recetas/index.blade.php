c9b6cf94493388bbe4b09846d90f26f3be7d95c1
LucioRocca <30579534+lucioRocca@users.noreply.github.com>
2020-11-06 14:58:13 -0300

    Update index.blade.php

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -42,15 +42,15 @@
                         </svg>
                     </a>
 
-                <form action={{route('recetasDestroy', ['receta' => $receta])}} id='{{'form-eliminar'}}' method="POST">
+                    <form action={{route('recetasDestroy', ['receta' => $receta])}} id="{{'form-eliminar-'.$receta->id}}" method="POST">
                         @csrf
                         @method('delete')
-                        <a   class="eliminar btn btn-danger w-100" >
+                        <button type="submit" form="{{'form-eliminar-'.$receta->id}}" class="eliminar btn btn-danger w-100" >
                             Eliminar&nbsp
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
                             </svg>
-                        </a>
+                        </button>
                     </form>
                 </td>
             </tr>
